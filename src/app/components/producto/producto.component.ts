20a906759a929c5a76d397746fa1f0ea16fd348b
Nahuel Alberto Cachambi <cachasan.10@gmail.com>
2025-04-24 01:42:00 -0300

    Se agrego las card de los productos Se agrego las imagenes de los productos Se agrego las funciones de sumar,restar cantidad

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -8,11 +8,52 @@ import { CommonModule } from '@angular/common';
   styleUrl: './producto.component.css'
 })
 export class ProductoComponent {
+  path = 'images/';
+
   productos: Producto[] = [
-   new Producto(1,  'Producto 1', 'Descripción del producto 1', 100, 'image/producto1.jpg', 'Descripción de la imagen del producto 1'),
-   new Producto(2,  'Producto 2', 'Descripción del producto 2', 200, 'image/producto2.jpg', 'Descripción de la imagen del producto 2'),
-   new Producto(3,  'Producto 3', 'Descripción del producto 3', 300, 'image/producto3.jpg', 'Descripción de la imagen del producto 3'), 
-   new Producto(4,  'Producto 4', 'Descripción del producto 4', 400, 'image/producto4.jpg', 'Descripción de la imagen del producto 4'),
-   new Producto(5,  'Producto 5', 'Descripción del producto 5', 500, 'image/producto5.jpg', 'Descripción de la imagen del producto 5'),
+   new Producto(1,  'Notebook Samsung Galaxy Book3', 'La Notebook Samsung Galaxy Book3 de 15.6" en color plateado ofrece un rendimiento potente gracias a su procesador Intel Core i7, 16GB de RAM y un rápido almacenamiento SSD de 512GB. Ideal para productividad y tareas exigentes en un diseño elegante y portátil.', 100, 1, this.path + 'netbook-samsung.webp', 'Notebook Samsung Galaxy Book3'),
+   new Producto(2,  'Parlante Philips TAX3705/77N', 'El parlante Philips TAX3705/77N es un altavoz portátil para fiestas que destaca por su sonido potente con graves definidos, luces de fiesta dinámicas, resistencia a salpicaduras, micrófono para karaoke, y batería de larga duración para disfrutar de la música en cualquier lugar. Ideal para animar reuniones y eventos.', 200, 1, this.path + 'parlante-philips.webp', 'Parlante Philips TAX3705/77N'),
+   new Producto(3,  'Celular Motorola G32', 'El Motorola G32 es un celular de gama media que destaca por su pantalla fluida de 90Hz, una batería de larga duración y una cámara principal de 50MP. Ofrece un buen rendimiento para tareas diarias y multimedia a un precio accesible.', 300, 1, this.path + 'celular-motorola.webp', 'Celular Motorola G32'), 
+   new Producto(4,  'Licuadora Atma LV24R1A', 'La licuadora Atma LV24R1A roja de 450W es un electrodoméstico de cocina para preparar licuados y batidos. Su diseño es compacto y de color rojo.', 400, 1, this.path + 'licuadora-atma.webp', 'Licuadora Atma LV24R1A'),
   ];
+
+  productosComprado: Producto[] = [];
+
+  agregarProducto(producto: Producto) { 
+    this.productosComprado.push(producto);
+  }
+
+  eliminarProducto(producto: Producto) {
+    this.productosComprado = this.productosComprado.filter(p => p !== producto);
+  }
+
+  sumarCantidadProducto(producto: Producto) {
+    this.productosComprado.forEach(p => {
+      if (p.id === producto.id) {
+        p.cantidad += 1;
+      }
+    });   
+  }
+
+  restarCantidadProducto(producto: Producto) {
+    this.productosComprado.forEach(p => {
+      if (p.id === producto.id) {
+        p.cantidad -= 1;
+      }
+    });
+    if (producto.cantidad === 0) {
+      this.eliminarProducto(producto);
+    }
+  } 
+
+  mostrarTotalProducto(producto: Producto) {
+    let total = 0;
+    this.productosComprado.forEach(p => { 
+      if (p.id === producto.id) {
+        total = p.precio * p.cantidad;
+      }
+    });
+    return total;
+  } 
+
 }
--- src/app/models/producto.ts
+++ src/app/models/producto.ts
@@ -3,14 +3,16 @@ export class Producto {
     nombre: string;
     descripcion: string;
     precio: number;
+    cantidad: number;
     imagen: string;
     imagen_descripcion: string;
     
-    constructor(id: number, nombre: string, descripcion: string, precio: number, imagen: string, imagen_descripcion: string) {
+    constructor(id: number, nombre: string, descripcion: string, precio: number, cantidad: number, imagen: string, imagen_descripcion: string) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
+        this.cantidad = cantidad;
         this.imagen = imagen;
         this.imagen_descripcion = imagen_descripcion;
     }   
